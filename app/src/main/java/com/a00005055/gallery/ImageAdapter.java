bc7637133ce100ef700bdf817516fc8d4a48f8b5
Jalol <kuldoshev171996@gmail.com>
2018-05-06 23:34:05 +0500

    Gallery App

---
+++ app/src/main/java/com/a00005055/gallery/ImageAdapter.java
@@ -0,0 +1,51 @@
+package com.a00005055.gallery;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+
+
+public class ImageAdapter extends BaseAdapter {
+
+    private Context context;
+
+    public Integer[] images = {
+            R.drawable.pic_1, R.drawable.pic_2,
+            R.drawable.pic_3, R.drawable.pic_4,
+            R.drawable.pic_5, R.drawable.pic_6,
+            R.drawable.pic_7, R.drawable.pic_8,
+            R.drawable.pic_9, R.drawable.pic_10,
+    };
+
+    public ImageAdapter(Context c){
+        context = c;
+    }
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return images[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = new ImageView(context);
+        imageView.setImageResource(images[position]);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+        imageView.setLayoutParams(new GridView.LayoutParams(240,240));
+        return imageView;
+    }
+}
